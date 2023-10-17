355080aeeec983a63d67216f3e032a9e40e634c1
Octavian <octavian.hagi-memet@edu.sait.ca>
2023-10-16 20:00:17 -0600

    Step 2 - Function converting C to Function

--- temp.py
+++ temp.py
@@ -1,3 +1,10 @@
 celsius = 25
 fahrenheit = celsius * 9/5 + 32
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
+
+def celsius_to_fahrenheit():
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+celsius_to_fahrenheit()
+print(f"fahrenheit: {fahrenheit}")

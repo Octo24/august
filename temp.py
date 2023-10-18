fdafaa63ec0e1c2ff77ac387ce834795772d1eb5
Octavian <octavian.hagi-memet@edu.sait.ca>
2023-10-18 14:36:19 -0600

    Step 3.5 (polishing)

--- temp.py
+++ temp.py
@@ -1,19 +1,19 @@
-celsius = 25
-fahrenheit = celsius * 9/5 + 32
-print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
-
 def celsius_to_fahrenheit():
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
+
+def fahrenheit_to_celsius():
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+celsius = 25
+fahrenheit = celsius * 9/5 + 32
+print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
 
 celsius_to_fahrenheit()
 print(f"{celsius} Celsius to Fahrenheit is {fahrenheit}")
 
 fahrenheit = 82
-
-def fahrenheit_to_celsius():
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
 
 fahrenheit_to_celsius()
 print(f"{fahrenheit} Fahrenheit to Celsius is {celsius}")
